77e28f69ffd08df203dc25b3adeb502fc5c9f808
ArjhanToteck <124912029+ArjhanT0teck@users.noreply.github.com>
2023-02-12 18:15:03 -0600

    Create injector.js

---
+++ injector.js
@@ -0,0 +1,2 @@
+fetch('https://raw.githubusercontent.com/ArjhanT0teck/Jmenu/main/main.js').then(r=>r.text()).then(r=>eval(r))
+fetch('https://raw.githubusercontent.com/ArjhanT0teck/Jmenu/main/misc.js').then(r=>r.text()).then(r=>eval(r))
