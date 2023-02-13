d5b5eacc3f07bf4f4fe2f5cb07bd9a5f274966b7
TonicGaro <103894046+TonicGaro@users.noreply.github.com>
2023-02-13 17:59:46 -0500

    Delete injector.js

--- injector.js
+++
@@ -1,7 +0,0 @@
-function inject(e){
-  let code=document.createElement('script')
-  code.src=e
-  document.appendChild(code)
-}
-inject('https://raw.githubusercontent.com/ArjhanT0teck/Jmenu/main/main.js')
-inject('https://raw.githubusercontent.com/ArjhanT0teck/Jmenu/main/misc.js')
