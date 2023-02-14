e8b6d13b8d6edc513d93a3a091e8140a15f35b7e
ArjhanToteck <124912029+ArjhanT0teck@users.noreply.github.com>
2023-02-14 17:13:59 -0600

    cheatcodes-retardness

--- src/misc/cheatcodes.js
+++ src/misc/cheatcodes.js
@@ -1,18 +1,21 @@
-// prompts user to insert cheat code
-var cheatprompt = prompt('Insert cheat code:')
+class cheats {
+    static name = function() {
+        // prompts user with what name they want
+        var nameprompt = prompt('What name do you want?', '<div class="css-1ii68kw e15psnz1">Insert name here:</div>')
 
-if (cheatprompt === 'dig') {
-    window.open('https://digsite.garoikana.repl.co/', '_blank');
-} 
+        // replaces name
+        setInterval(function() {
+            try {
+                document.getElementById('firstname').innerHTML = nameprompt
+            } catch (e) {}
+        }, 1);
+    }
+    static dig = function() {
+        window.open('https://digsite.garoikana.repl.co/', '_blank');
 
-if (cheatprompt === 'name') {
-    customName()
+    }
 }
-
-function customName() {
-// prompts user with what name they want
-var nameprompt = prompt('What name do you want?', '<div class="css-1ii68kw e15psnz1">Insert name here:</div>')
-
-// replaces name
-setInterval(function () { try{document.getElementById('firstname').innerHTML = nameprompt}catch(e){} }, 1);
-}
+let p = prompt("Enter cheat code here:");
+if (typeof cheats[p] !== "undefined") {
+    cheats()
+} else alert("Cheat doesn't exist")
