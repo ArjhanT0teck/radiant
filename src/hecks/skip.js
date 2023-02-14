5df83f267b82052b43063e15ad50adf5b7373c9b
ArjhanToteck <124912029+ArjhanT0teck@users.noreply.github.com>
2023-02-14 17:47:54 -0600

    Update skip.js

--- src/hecks/skip.js
+++ src/hecks/skip.js
@@ -4,11 +4,11 @@ if (window['html5Iframe'] === undefined || window['html5Iframe'] === null || win
     alert('Please open a lesson.')
 } else { // prompts user what score they want
   alert('Make sure you farm minutes before skipping! Teachers can see how much time you spend in a lesson.')  
-  var skip = prompt('What score do you want?')
+  var score = html5Iframe.src.split("?csid=")[1].split("&type")[0].includes("10_") && document.getElementsByTagName("iframe")[0].id != "closereading_lesson" ?prompt("What score do you want?"): alert("This doesn't work for close reading.") 
       
-  if (skip === undefined || skip === null) { // cancels skipper if nothing is entered
-    console.log('Skipper canceled.')
+  if (score === undefined || score === null) { // cancels skipper if nothing is entered
+    alert("Cannot be nothing.")
 } else { // skips lesson with inputted score
-    p1(skip)
+    p1(score)
   }
 }
