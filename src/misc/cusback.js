1fc99b1c527949427d7af21e2b6a5fe9ee5bc4ba
ArjhanToteck <124912029+ArjhanT0teck@users.noreply.github.com>
2023-02-14 17:52:53 -0600

    Update cusback.js

--- src/misc/cusback.js
+++ src/misc/cusback.js
@@ -1,6 +1,6 @@
 // prompts user with image address, replaces img src with inputted image address
-var CustomImage = prompt('Paste your custom image address here: (Recommended dimensions are 1020 x 603)')
-setInterval(function () { try{document.getElementById("background-image").src=CustomImage;}catch(e){} }, 1);
+var CustomImage = prompt('Paste your custom image address here: (Recommended dimensions are '+innerWidth+' x '+innerHeight+')')
+    localStorage.setItem('background',CustomImage)
 
 if (CustomImage === undefined || CustomImage === null) { // reloads i-ready if prompt is canceled
   location.reload()
