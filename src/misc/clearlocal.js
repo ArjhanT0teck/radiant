afafd6fefd2850897d665b6e489462ce18bf5a47
ArjhanToteck <124912029+ArjhanT0teck@users.noreply.github.com>
2023-03-05 12:39:42 -0600

    Delete src directory

--- src/misc/clearlocal.js
+++
@@ -1,9 +0,0 @@
-var clearconfirm = confirm('Are you sure you want to clear localStorage? All saved data like custom coins, backgrounds, etc, will be cleared.');
-if (clearconfirm === true) {
-    localStorage.clear();
-    alert('localStorage Cleared!')
-}
-
-if (clearconfirm === false) {
-    alert('Canceled.')
-}
--- src/misc/cusback.js
+++
@@ -1,7 +0,0 @@
-// prompts user with image address, replaces img src with inputted image address
-var CustomImage = prompt('Paste your custom image address here: (Recommended dimensions are '+innerWidth+' x '+innerHeight+')')
-    localStorage.setItem('background',CustomImage)
-
-if (CustomImage === undefined || CustomImage === null) { // reloads i-ready if prompt is canceled
-  location.reload()
-}
